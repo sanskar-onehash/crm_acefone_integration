3c398911cd71b7a86d03ad5b39ab2b052314cdb9
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-28 07:40:30 +0530

    feat(user): setup dynamic options per row, for popup subscriptions
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_acefone_integration/acefone/doctype/acefone_user/acefone_user.js
+++ crm_acefone_integration/acefone/doctype/acefone_user/acefone_user.js
@@ -1,8 +1,67 @@
 // Copyright (c) 2025, OneHash and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Acefone User", {
-// 	refresh(frm) {
+const phoneFieldsMap = {};
 
-// 	},
+// frappe.ui.form.on("Acefone User", {
+//   refresh(frm) {
+//
+//   },
 // });
+
+frappe.ui.form.on("Acefone DocType Subcriptions", {
+  subscribed_for: async function (frm, cdt, cdn) {
+    const row = frappe.get_doc(cdt, cdn);
+    const gridRow = frm.fields_dict.call_subscriptions.grid.get_grid_row(
+      row.name,
+    );
+    const phoneFieldControl = gridRow.columns.phone_field.field;
+
+    if (row.subscribed_for) {
+      frappe.model.with_doctype(row.subscribed_for, () => {
+        const docMeta = frappe.get_meta(row.subscribed_for);
+        const phoneFields = getPhoneFields(docMeta.fields).map((df) => ({
+          fieldname: df.fieldname,
+          label: df.label,
+        }));
+        const labels = [];
+
+        for (let field of phoneFields) {
+          labels.push(field.label);
+          if (!phoneFieldsMap[row.subscribed_for]) {
+            phoneFieldsMap[row.subscribed_for] = {};
+          }
+          phoneFieldsMap[row.subscribed_for][field.label] = field.fieldname;
+        }
+
+        phoneFieldControl.df.read_only = 0;
+        phoneFieldControl.df.options = labels;
+        phoneFieldControl.set_options();
+        phoneFieldControl.refresh();
+      });
+    } else {
+      frappe.model.set_value(cdt, cdn, "phone_field", "");
+      frappe.model.set_value(cdt, cdn, "phone_fieldname", "");
+      phoneFieldControl.df.read_only = 1;
+      phoneFieldControl.refresh();
+    }
+  },
+  phone_field: function (frm, cdt, cdn) {
+    const row = frappe.get_doc(cdt, cdn);
+    frappe.model.set_value(
+      cdt,
+      cdn,
+      "phone_fieldname",
+      phoneFieldsMap[row.subscribed_for][row.phone_field],
+    );
+  },
+});
+
+function getPhoneFields(docfields) {
+  return docfields.filter(
+    (df) =>
+      (df.fieldtype === "Phone" ||
+        (df.fieldtype === "Data" && df.options === "Phone")) &&
+      !df.hidden,
+  );
+}
