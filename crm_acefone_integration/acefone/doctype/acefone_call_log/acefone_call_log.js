8e040f9dabc29c17a52c558f3f687d2f289951bb
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-28 06:31:01 +0530

    feat(call_log): add play recording btn
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_acefone_integration/acefone/doctype/acefone_call_log/acefone_call_log.js
+++ crm_acefone_integration/acefone/doctype/acefone_call_log/acefone_call_log.js
@@ -1,8 +1,49 @@
 // Copyright (c) 2025, OneHash and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Acefone Call Log", {
-// 	refresh(frm) {
+frappe.ui.form.on("Acefone Call Log", {
+  refresh(frm) {
+    addCallLogsActions(frm);
+  },
+});
 
-// 	},
-// });
+function addCallLogsActions(frm) {
+  if (!frm.is_new()) {
+    addPlayRecordingBtn(frm);
+  }
+}
+
+function addPlayRecordingBtn(frm) {
+  if (!frm.doc.recording_url) {
+    return;
+  }
+  const BTN_LABEL = "Play Recording";
+  frm.page.remove_inner_btn(BTN_LABEL);
+  frm.page.add_inner_btn(
+    BTN_LABEL,
+    () => playAudioPopup(frm.doc.recording_url),
+    null,
+    "primary",
+  );
+}
+
+function playAudioPopup(audioUrl) {
+  if (!audioUrl) {
+    frappe.msgprint(__("No audio URL found in the document."));
+    return;
+  }
+
+  const audio_html = `
+        <audio controls autoplay style="width: 100%;">
+            <source src="${audioUrl}" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+    `;
+
+  frappe.msgprint({
+    title: __("Audio Player"),
+    indicator: "blue",
+    message: audio_html,
+    wide: true,
+  });
+}
