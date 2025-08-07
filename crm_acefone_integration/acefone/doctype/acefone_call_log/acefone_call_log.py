4533ab3f989adcfa10fd731a977e61beaed0d422
Sanskar Soni <sanskarsoni300@gmail.com>
2025-08-07 10:47:59 +0530

    feat(call_log:api): add endpoint to fetch doc related call logs
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_acefone_integration/acefone/doctype/acefone_call_log/acefone_call_log.py
+++ crm_acefone_integration/acefone/doctype/acefone_call_log/acefone_call_log.py
@@ -1,9 +1,37 @@
 # Copyright (c) 2025, OneHash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+CALL_LOGS_FETCH_LIMIT = 50
 
 
 class AcefoneCallLog(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def get_call_logs(doctype, docname, page=0):
+    if not doctype or not docname:
+        return []
+
+    return frappe.get_list(
+        "Acefone Call Log",
+        filters={
+            "call_for_doc": doctype,
+            "linked_doc": docname,
+        },
+        fields=[
+            "call_type",
+            "customer_phone",
+            "caller_id",
+            "call_status",
+            "start_stamp",
+            "duration",
+            "recording_url",
+        ],
+        order_by="start_stamp desc",
+        limit_start=page * CALL_LOGS_FETCH_LIMIT,
+        limit_page_length=CALL_LOGS_FETCH_LIMIT,
+    )
