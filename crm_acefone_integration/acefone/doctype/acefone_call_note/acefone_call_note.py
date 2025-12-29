bb475c4e549a3af4b654302d94f6366a48330ae0
Sanskar Soni <sanskarsoni300@gmail.com>
2025-12-29 11:39:31 +0530

    fix(call_note): add call note to call log if note added after log
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- crm_acefone_integration/acefone/doctype/acefone_call_note/acefone_call_note.py
+++ crm_acefone_integration/acefone/doctype/acefone_call_note/acefone_call_note.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2025, OneHash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AcefoneCallNote(Document):
-	pass
+
+    def before_insert(self):
+        call_log = frappe.db.exists(
+            "Acefone Call Log",
+            {
+                "call_id": self.call_id,
+                "uuid": self.uuid,
+                "call_note": ["is", "not set"],
+            },
+        )
+        if call_log:
+            frappe.db.set_value("Acefone Call Log", call_log, "call_note", self.name)
